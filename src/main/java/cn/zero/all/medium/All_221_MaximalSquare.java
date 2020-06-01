e02ce296d20e425429996358a16acc1cd5fcae5c
Zero5269 <945246041@qq.com>
2020-06-01 23:26:13 +0800

    add code

---
+++ src/main/java/cn/zero/all/medium/All_221_MaximalSquare.java
@@ -0,0 +1,60 @@
+package cn.zero.all.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 221. 最大正方形
+ * <p>
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * <p>
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * <p>
+ * 输出: 4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class All_221_MaximalSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+
+    @Test
+    public void test() {
+        char[][] matrix = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        int i = maximalSquare(matrix);
+        Assert.assertEquals(i, 4);
+        System.out.println(i);
+    }
+
+
+}
